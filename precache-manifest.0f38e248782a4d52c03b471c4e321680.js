bea2b7ab1ce12f5125539da7d8eecabff7368fef
Sergey Rumiantsev <grapefruitcompany@gmail.com>
2019-03-05 06:21:23 +0200

    Updates

---
+++ precache-manifest.0f38e248782a4d52c03b471c4e321680.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f04d14755a9322203956",
+    "url": "/museum/static/js/runtime~main.f04d1475.js"
+  },
+  {
+    "revision": "3d981ec72cbe1f16f03c",
+    "url": "/museum/static/js/main.3d981ec7.chunk.js"
+  },
+  {
+    "revision": "0c2587fb8c225727d196",
+    "url": "/museum/static/js/2.0c2587fb.chunk.js"
+  },
+  {
+    "revision": "3d981ec72cbe1f16f03c",
+    "url": "/museum/static/css/main.5198a776.chunk.css"
+  },
+  {
+    "revision": "87cdb79d02a6019105ce73e5c9d81cb1",
+    "url": "/museum/index.html"
+  }
+];
